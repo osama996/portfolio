4f0ce8f62ac93176816ba57248df6309171de125
osama <enlil96rashid@gmail.com>
2018-06-05 13:54:47 +0200

    set the working Environment

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid p-3">
+        Holaaa
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App";
+import "font-awesome/css/font-awesome.css";
+
+render(<App />, document.querySelector("#main"));
